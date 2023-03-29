9b1ca8ae8c13111ffc9352598e20277d0e1a78c7
Yusuf <yusufdavid411@gmail.com>
2023-03-28 18:22:15 -0700

    file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,12 @@
 #include "main.h"
-/**
- * _puts - prints strings
- * @str: function parameter
- * Return: 0
- */
 
-int _strlen(char *str)
+int _strlen(char *s)
 {
 	int i;
 
 	for (i = 0; str[i] != '/0' : i++)
-		_putchar(str[i]);
-	_putchar('/n');
+	{
+		count++;
+	}
+	return (count);
 }
