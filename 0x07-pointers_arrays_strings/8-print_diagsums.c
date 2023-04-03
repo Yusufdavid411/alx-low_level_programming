446bdb4628cec42fd3c9e2469af2185eba91cbed
Yusuf <yusufdavid411@gmail.com>
2023-04-03 16:07:27 -0700

    files

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagrams - Entry point
+ * print_diagsums - Entry point
  * @a: input
  * @size: input
  * Return: Always 0 (success)
  */
-void print_diagrams(int *a, int size)
+void print_diagsums(int *a, int size)
 {
 	int sum1, sum2, y;
 
